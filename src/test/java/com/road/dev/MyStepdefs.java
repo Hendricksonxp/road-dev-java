022f181e58a0397b34bd77b9a8c89110211a8da0
Nayan Hajratwala <nayan@chikli.com>
2018-09-01 00:10:31 -0400

    tests go much faster if you take the sleep out :)

--- src/test/java/com/road/dev/MyStepdefs.java
+++ src/test/java/com/road/dev/MyStepdefs.java
@@ -2,6 +2,7 @@ package com.road.dev;
 
 import com.road.dev.pages.LoginPage;
 import com.road.dev.pages.WelcomePage;
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -11,28 +12,31 @@ import org.junit.Ignore;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @Ignore
 public class MyStepdefs extends SpringIntegrationTest {
 
-    private final static WebDriver driver = new ChromeDriver();
-
+    private WebDriver driver;
     private LoginPage loginPage;
     private WelcomePage welcomePage;
 
     @Before
     public void initializePageObjects() {
+        driver = new ChromeDriver();
         loginPage = new LoginPage(driver, port);
         welcomePage = new WelcomePage(driver);
+    }
+
+    @After
+    public void shutdownBrowser() {
+        driver.quit();
     }
 
     @Given("^I am on the login page$")
     public void iAmOnTheLoginPage() {
         loginPage.visit();
-        loginPage.sleep(3);
     }
 
     @When("^I enter a valid username and password$")
@@ -42,7 +46,7 @@ public class MyStepdefs extends SpringIntegrationTest {
 
     @Then("^I should see the welcome page$")
     public void iShouldSeeTheWelcomePage() {
-        assertTrue(welcomePage.title().contains("Welcome"));
+        assertThat(welcomePage.title()).contains("Welcome");
     }
 
     @When("^I enter an invalid username or password$")
@@ -52,12 +56,12 @@ public class MyStepdefs extends SpringIntegrationTest {
 
     @Then("^I expect to be on the login page$")
     public void iExpectToBeOnTheLoginPage() {
-        assertTrue(loginPage.title().contains("Login"));
+        assertThat(loginPage.title()).contains("Login");
     }
 
     @And("^I expect to see an error message$")
     public void iExpectToSeeAnErrorMessage() {
-        assertTrue(loginPage.message().contains("Incorrect Username/Password"));
+        assertThat(loginPage.message()).contains("Incorrect Username/Password");
     }
 
     @When("^I am missing a username$")
@@ -67,6 +71,6 @@ public class MyStepdefs extends SpringIntegrationTest {
 
     @And("^I expect to see an missing field error$")
     public void iExpectToSeeAnMissingFieldError() {
-        assertEquals("Missing Username", loginPage.message());
+        assertThat("Missing Username").isEqualTo(loginPage.message());
     }
 }
